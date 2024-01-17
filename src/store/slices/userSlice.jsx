28ca3565926291e527a7f6183fb598a5328439d6
Adem Karagülle <ademkaragulle@outlook.com>
2024-01-17 14:37:37 +0300

    delete unnessary comments line

--- src/store/slices/userSlice.jsx
+++ src/store/slices/userSlice.jsx
@@ -13,8 +13,7 @@ export const login = createAsyncThunk("login", async (user) => {
 
         if (response.ok) {
             const data = await response.json();
-            sessionStorage.setItem('authToken', data.authToken)
-            return data.authToken
+            return data
         } else {
             console.error('Login failed');
         }
@@ -23,79 +22,65 @@ export const login = createAsyncThunk("login", async (user) => {
     }
 });
 
-
-export const getUserInformation = createAsyncThunk("getUserInformation", async () => {
-    let data;
-    const storedDataString = sessionStorage.getItem("authToken");
-    if (storedDataString) {
-        try {
-            const response = await fetch(
-                "https://localhost:7267/api/Auth/LoginUser",
-                {
-                    method: "GET",
-                    headers: {
-                        "Content-Type": "application/json",
-                        Accept: "text/plain",
-                        Authorization: `Bearer ${storedDataString}`,
-                    },
-                }
-            )
-            data = await response.json()
-            if (!response.ok) {
-                throw new Error("API isteği başarısız");
-            }
-
-        } catch (error) {
-            console.error("Hata:", error.message);
-        }
+export const signUp = createAsyncThunk("signUp", async (newUser) => {
+    try {
+        const response = await axios.post('https://localhost:7267/registerUser', newUser);
+        return response.data
+    } catch (error) {
+        throw error;
     }
-    return data
 });
-
-
 
 const userSlice = createSlice({
     name: 'user',
     initialState: {
         users: [],
         currentUser: null,
-        token: null
+        token: null,
     },
     reducers: {
-        SignUp: async (state, action) => {
-            const users = await axios.get("http://localhost:3000/users")
-            const newUser = action.payload;
-            const existingUser = users.data.find(item => item.userName === newUser.userName);
-            if (!existingUser) {
-                axios.post('http://localhost:3000/users', newUser)
-                    .then(response => {
-                        console.log('POST response: Başarılı');
-                    })
-                    .catch(error => {
-                        console.error('POST error:', error);
-                    });
-            } else {
-                alert("This user name is used")
-            }
-        },
         Logout: (state, action) => {
             sessionStorage.removeItem('authToken')
+            sessionStorage.removeItem('user')
             state.currentUser = null
+            state.token = null
+        },
+        IsExistToken: (state, action) => {
+            state.currentUser = JSON.parse(action.payload.user)
+            state.token = action.payload.token
         }
     },
     extraReducers: (builder) => {
         builder.addCase(login.fulfilled, (state, action) => {
-            state.token = action.payload;
+            if (action.payload.username) {
+                const newUser = {
+                    username: action.payload.username,
+                    email: action.payload.email,
+                    password: action.payload.password,
+                }
+                state.token = action.payload.authToken;
+                state.currentUser = newUser;
+                sessionStorage.setItem('authToken', action.payload.authToken);
+                sessionStorage.setItem('user', JSON.stringify(newUser));
+            }
         });
-        builder.addCase(getUserInformation.fulfilled, (state, action) => {
-            console.log(action.payload)
-            state.currentUser = action.payload.username;
+        builder.addCase(signUp.fulfilled, (state, action) => {
+            if (action.payload) {
+                alert("Kayıt İşlemi Başarılı")
+            }
+            else {
+                alert("kayıt işlemi başarısız. başka bir username ile tekrar deneyiniz.")
+            }
+        });
+
+        builder.addCase(signUp.rejected, (state, action) => {
+            console.error('Kullanıcı kaydı başarısız:', action.error.message);
         });
     },
 });
 
 export const {
-    SignUp,
+    IsExistToken,
     Logout
 } = userSlice.actions;
 
